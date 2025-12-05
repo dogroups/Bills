796fbeb41dcc88ee3f3bad7ca20ae623de133e81
dogroups <dogroups24@gmail.com>
2025-12-05 17:18:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-    console.log(`API available at http://localhost:${PORT}/api`);
+    console.log(`API available at https://bills-psi.vercel.app:${PORT}/api`);
 });
